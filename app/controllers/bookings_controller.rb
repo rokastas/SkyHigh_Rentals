55cab4238535386efb1fb0b41b5185e95cf5bd8e
willem kriek <kriek.wj@gmail.com>
2024-01-30 13:27:42 +0100

    added edit and update booking in controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -6,8 +6,23 @@ class BookingsController < ApplicationController
   end
 
   def edit
+    @booking = Booking.find(params[:id])
+    @drone = @booking.drone
   end
 
   def update
+    @booking = Booking.find(params[:id])
+
+    if @booking.update(booking_params)
+      redirect_to drone_path(@drone), notice: 'Booking was successfully updated.'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:from_date, :to_date, :accepted)
   end
 end
