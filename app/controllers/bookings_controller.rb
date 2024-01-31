4646cc4e8d60fac722e455a3ba8447437039a8dd
Rokas Stasiulis <rokastas@gmail.com>
2024-01-31 15:30:07 +0100

    adding a booking in progress

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,13 +1,30 @@
 class BookingsController < ApplicationController
   def new
+    @booking = Booking.new
+    @drone = Drone.find(params[:drone_id])
   end
 
   def create
+    @booking = Booking.new(booking_params)
+    @drone = Drone.find(params[:drone_id])
+    @booking.drone = @drone
+    @booking.user = current_user
+    if @booking.save
+      redirect_to drones_path
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:from_date, :to_date)
   end
 end
