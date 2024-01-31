58f3ec0e857a0ae61bae6408d34d10ac80b21e7e
Tensae Mamo <tensaemamo@protonmail.com>
2024-01-31 14:12:29 +0100

    first v of navbar, sign out redirect to rootpath

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,21 @@ class ApplicationController < ActionController::Base
     # For additional in app/views/devise/registrations/edit.html.erb
     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :address, :bank_details])
   end
+
+  private
+
+  # TODO: uncomment once dashboard is implemented
+  # def after_sign_in_path_for(resource_or_scope)
+  #   dashboard_path
+  # end
+
+  def after_sign_out_path_for(resource_or_scope)
+    root_path
+  end
+
+  # TODO: uncomment once dashboard is implemented
+  # def after_log_in_path_for(resource_or_scope)
+  #   dashboard_path
+  # end
+
 end
