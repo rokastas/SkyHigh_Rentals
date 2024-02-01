d69251597096179d6ebfd88bcadcd9e303ff739a
Norbert Skwierczynski <norbert.fe.dev@gmail.com>
2024-02-01 13:47:35 +0000

    finish search by category and price

--- app/controllers/drones_controller.rb
+++ app/controllers/drones_controller.rb
@@ -3,9 +3,20 @@ class DronesController < ApplicationController
 
   def index
     @drones = Drone.all
+    filter_drones
   end
 
   def show
     @drone = Drone.find(params[:id])
+  end
+
+  private
+
+  def filter_drones
+    @drones = Drone.all
+    @drones = @drones.where("category ILIKE :query OR price ILIKE :query", query: "%#{params[:query]}%") if params[:query].present?
+    @drones = @drones.where(category: params[:category]) if params[:category].present?
+    @drones = @drones.where("price >= ?", params[:min_price]) if params[:min_price].present?
+    @drones = @drones.where("price <= ?", params[:max_price]) if params[:max_price].present?
   end
 end
