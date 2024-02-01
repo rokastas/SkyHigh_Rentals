018e8175ee5ef186331848827d4e748f24724813
Rokas Stasiulis <rokastas@gmail.com>
2024-02-01 13:42:16 +0100

    dashboard filtering in progress

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -2,8 +2,20 @@ class DashboardsController < ApplicationController
   before_action :user_dashboard, only: [:dashboard]
 
   def dashboard
-    # @user = current_user
+    @user = current_user
+    @drones = Drone.all
+    @user_drones = @user.drones
+    @user_drones_ids = @user.drones.ids
+    @user_bookings = @user.bookings
     # @user_booked_drones = @user.booked_drones.includes(:drone)
+      # get all bookings where my drones are booked
+    @all_bookings = Booking.all
+    @my_drones_bookings = []
+
+    @user_drones_ids.each do |drone_id|
+      @my_drones_bookings << @all_bookings.where(drone_id: drone_id)
+    end
+      # @bookings_of_my_drones = current_user.booked_drone_ids
   end
 
   private
@@ -13,3 +25,5 @@ class DashboardsController < ApplicationController
     @all_drones_grouped_by_user = Drone.includes(bookings: :user).order(order_options).group_by { |drone| drone.user }
   end
 end
+
+# filter bookings by
