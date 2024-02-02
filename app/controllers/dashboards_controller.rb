071be50775f8d06c5005140458b366331eace6d4
Tensae Mamo <tensaemamo@protonmail.com>
2024-02-02 12:06:05 +0100

    added cancel booking if booking accepted, sorted bookings by date in dashboard

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -2,7 +2,7 @@ class DashboardsController < ApplicationController
   def index
     @user = current_user
 
-    @my_requests = Booking.where(drone: Drone.where(user: @user)).where(accepted: false)
+    @my_requests = Booking.where(drone: Drone.where(user: @user))
     @my_drones = Drone.where(user: @user)
     @my_bookings = Booking.where(user: @user)
   end
